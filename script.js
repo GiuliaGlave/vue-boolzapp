c780323a85dfb13111660de291f9fabf1bc2445d
GiuliaGlave <giulia.glave@gmail.com>
2023-02-16 21:40:20 +0100

    milestone 3

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 //● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 //● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
-Milestone 2
+//Milestone 2
 //● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 //● Click sul contatto mostra la conversazione del contatto cliccato
 
-Milestone 3
-● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-“enter” il testo viene aggiunto al thread sopra, come messaggio verde
-● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+//Milestone 3
+//● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+//● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
 Milestone 4
 ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
@@ -185,12 +184,37 @@ const app = createApp({
         },
       ],
       activeContact: 0,
+
+      newMessage: [
+        {
+          date: "10/01/2020 15:30:55",
+          message: "",
+          status: "sent",
+        },
+      ],
     };
   },
 
   methods: {
     clickContact(i) {
       this.activeContact = i;
+    },
+
+    sendMessage() {
+      this.contacts[this.activeContact].messages.push({
+        //vedi opzione ...this.newMessage
+        date: "10/01/2020 15:30:55",
+        message: this.newMessage.message,
+        status: "sent",
+      });
+
+      setTimeout(() => {
+        this.contacts[this.activeContact].messages.push({
+          date: "10/01/2020 15:30:55",
+          message: "ok",
+          status: "received",
+        });
+      }, 1000);
     },
   },
 });
